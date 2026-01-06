ba4095ffdd743c0c819a10b3c46fdfdf45a8d25d
abubakar-wxy <abubakar.all.dv@gmail.com>
2026-01-06 18:31:43 +0600

    home page ui changes

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -20,15 +20,11 @@ export default function Footer() {
 
   const company = [
     { name: "About Us", href: "/about" },
-    { name: "Our Team", href: "/about#team" },
-    { name: "Projects", href: "/projects" },
-    { name: "Clients", href: "/clients" },
-    { name: "Partners", href: "/partners" },
   ];
 
   return (
     <footer className="bg-gradient-to-b from-pearl-aqua-500 to-pearl-aqua-800 text-white">
-      <div className="container px-6 md:px-10 mx-auto py-16">
+      <div className="container px-6 md:px-10 mx-auto pt-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
           
           {/* Company Info */}
@@ -128,6 +124,7 @@ export default function Footer() {
                   href="mailto:tlc@tlcbd.com"
                   className="w-10 h-10 rounded-full bg-pearl-aqua-700 hover:bg-pearl-aqua-600 flex items-center justify-center transition-colors"
                   aria-label="Email"
+                  target="_blank"
                 >
                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
@@ -138,6 +135,7 @@ export default function Footer() {
                   href="https://bd.linkedin.com/company/tradelinkers"
                   className="w-10 h-10 rounded-full bg-pearl-aqua-700 hover:bg-pearl-aqua-600 flex items-center justify-center transition-colors"
                   aria-label="LinkedIn"
+                  target="_blank"
                 >
                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
@@ -147,6 +145,7 @@ export default function Footer() {
                   href="https://www.facebook.com/tlebd"
                   className="w-10 h-10 rounded-full bg-pearl-aqua-700 hover:bg-pearl-aqua-600 flex items-center justify-center transition-colors"
                   aria-label="Facebook"
+                  target="_blank"
                 >
                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
@@ -162,15 +161,7 @@ export default function Footer() {
         {/* Bottom Bar */}
         <div className="mt-12 pt-8 border-t border-pearl-aqua-800">
           <div className="text-center flex-col md:flex-row  gap-4 text-sm text-pearl-aqua-300">
-            <p>© 2025 Tradelinkers Engineering Ltd. All rights reserved.</p>
-            {/* <div className="flex gap-6">
-              <Link href="/privacy" className="hover:text-pearl-aqua-100 transition-colors">
-                Privacy Policy
-              </Link>
-              <Link href="/terms" className="hover:text-pearl-aqua-100 transition-colors">
-                Terms of Service
-              </Link>
-            </div> */}
+            <p className="pb-8">© 2025 Tradelinkers Engineering Ltd. All rights reserved.</p>
           </div>
         </div>
       </div>
